623d4e8e9edafa35bd4aa48b41bf3597d7c0afe0
Amos L King <amos.l.king@gmail.com>
2015-12-29 14:16:31 -0600

    Removes blank affirmations
    Sometimes the user received a blank line instead of an affirmation.
    While in there I changed `shuffle.first` to `sample` to be more
    idiomatic and succinct.
    
    Amos King @adkron <amos@binarynoggin.com>

--- Rakefile
+++ Rakefile
@@ -1,2 +1,9 @@
 require "bundler/gem_tasks"
 
+task :console do
+  require 'irb'
+  require 'irb/completion'
+  ARGV.clear
+  IRB.start
+  require 'irb_affirmations'
+end
--- lib/irb_affirmations.rb
+++ lib/irb_affirmations.rb
@@ -3,17 +3,15 @@ require "irb_affirmations/version"
 
 module IrbAffirmations
   module PromptReturn
-    AFFIRMATIONS = %{
-You are worthy and deserving of beauty and well being in my life.
+    AFFIRMATIONS = %{You are worthy and deserving of beauty and well being in my life.
 You love what you do and you do what you love.
 You are connected to the source energy which provides within you the divine abilities to make very successful personal, financial and professional decisions.
 You are articulate and you have excellent communication skills.
-You communicate in a persuasive, charming and charismatic manner.
-    }.lines
+You communicate in a persuasive, charming and charismatic manner.}.lines
 
     def self.write(results)
       puts results
-      puts AFFIRMATIONS.shuffle.first
+      puts AFFIRMATIONS.sample
     end
   end
 end
